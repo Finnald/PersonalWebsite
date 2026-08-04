9cb49735071e079335b0a389911b83325a9cbbac
Finnald <copsnrobbers95@gmail.com>
2026-08-04 10:55:16 +0800

    better hobby cards

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
 
         <h1 className="text-4xl font-semibold pb-10 pt-20">What else do I do?</h1>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 items-start">
+        <div className="grid grid-cols-1 xl:grid-cols-3 gap-5 jusitfy-center">
           {data.hobbies.map(hobby => (
             <Hobby category={hobby.category} name={hobby.name} desc={hobby.description} image={hobby.image} />
           ))}
